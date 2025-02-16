c94740bbb6267c379be775be99a2428ed50f8853
Suehyun666 <em0200878@gmail.com>
2025-02-17 00:33:13 +0900

    fix: 충돌해결을 위한 따옴표 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from './pages/Landing';
-import Main from './pages/Main';
-import Join from './pages/Join';
-import ResultReport from './pages/resultReport';
-import RecordingPage from './pages/RecordingPage';
-import PlaylistInner from './PlPage/PlaylistInner';
-import PlaylistPage from './PlPage/PlaylistPage';
-import ProtectedRoute from './routes/ProtectedRoute';
-import NotFound from './pages/NotFound';
-import MyPage from './pages/MyPage.jsx';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Main from "./pages/Main";
+import Join from "./pages/Join";
+import ResultReport from "./pages/resultReport";
+import RecordingPage from "./pages/RecordingPage";
+import PlaylistInner from "./PlPage/PlaylistInner";
+import PlaylistPage from "./PlPage/PlaylistPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import NotFound from "./pages/NotFound";
+import MyPage from "./pages/MyPage.jsx";
 
 const App = () => {
   return (
